1c6f9b62f14c5eede959a0d90155cfff81960f72
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-02-15 07:51:24 -0500

    Creeando conexion

---
+++ Fargoapi/Fargoapi/Conexion/ConexionDB.cs
@@ -0,0 +1,19 @@
+﻿namespace Fargoapi.Conexion
+{
+    public class ConexionDB
+    {
+        private String Conexion = String.Empty;
+        public ConexionDB() { 
+        var builder = new ConfigurationBuilder().SetBasePath
+                (Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").
+                Build();
+            Conexion = builder.GetSection
+                    ("ConnectionStrings:conexion").Value;
+        }
+        public String cadena() {
+            return Conexion;
+        }
+        
+        
+    }
+}
