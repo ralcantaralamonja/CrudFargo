d257f8e78042322922f4b12ec163c5afcec8e0e5
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-02-15 10:35:13 -0500

    Agregando manejo de errores

--- Fargoapi/Fargoapi/Datos/Dcontainers.cs
+++ Fargoapi/Fargoapi/Datos/Dcontainers.cs
@@ -8,45 +8,63 @@ namespace Fargoapi.Datos
     public class Dcontainers
     {
         ConexionDB cn = new ConexionDB();
+
+ 
+
         public async Task<List<Mcontenedor>> ListarContenedores()
         {
             var lista = new List<Mcontenedor>();
 
-            using (var sql = new SqlConnection(cn.cadena()))
+            try
             {
-                using (var cmd = new SqlCommand("usp_ListarContenedores", sql))
+                using (var sql = new SqlConnection(cn.cadena()))
                 {
                     await sql.OpenAsync();
-                    cmd.CommandType = CommandType.StoredProcedure;
 
-                    using (var contenedores = await cmd.ExecuteReaderAsync())
+                    using (var cmd = new SqlCommand("usp_ListarContenedores", sql))
                     {
-                        while (await contenedores.ReadAsync())
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        using (var contenedores = await cmd.ExecuteReaderAsync())
                         {
-                            var mcontenedores = new Mcontenedor();
-                            mcontenedores.idContenedor = contenedores.GetInt32(contenedores.GetOrdinal("idContenedor"));
-                            mcontenedores.numContenedor = contenedores.GetInt32(contenedores.GetOrdinal("numContenedor"));
-                            mcontenedores.TipContenedor = contenedores.GetString(contenedores.GetOrdinal("Tipo"));
-                            mcontenedores.tamContenedor = contenedores.GetInt32(contenedores.GetOrdinal("tamContenedor"));
-
-                            if (!contenedores.IsDBNull(contenedores.GetOrdinal("pesoContenedor")))
+                            while (await contenedores.ReadAsync())
                             {
-                                mcontenedores.pesoContenedor = contenedores.GetDouble(contenedores.GetOrdinal("pesoContenedor"));
-                            }
+                                var mcontenedores = new Mcontenedor();
+                                mcontenedores.idContenedor = contenedores.GetInt32(contenedores.GetOrdinal("idContenedor"));
+                                mcontenedores.numContenedor = contenedores.GetInt32(contenedores.GetOrdinal("numContenedor"));
+                                mcontenedores.TipContenedor = contenedores.GetString(contenedores.GetOrdinal("Tipo"));
+                                mcontenedores.tamContenedor = contenedores.GetInt32(contenedores.GetOrdinal("tamContenedor"));
 
-                            if (!contenedores.IsDBNull(contenedores.GetOrdinal("taraContenedor")))
-                            {
-                                mcontenedores.taraContenedor = contenedores.GetDouble(contenedores.GetOrdinal("taraContenedor"));
-                            }
+                                if (!contenedores.IsDBNull(contenedores.GetOrdinal("pesoContenedor")))
+                                {
+                                    mcontenedores.pesoContenedor = contenedores.GetDouble(contenedores.GetOrdinal("pesoContenedor"));
+                                }
 
-                            lista.Add(mcontenedores);
+                                if (!contenedores.IsDBNull(contenedores.GetOrdinal("taraContenedor")))
+                                {
+                                    mcontenedores.taraContenedor = contenedores.GetDouble(contenedores.GetOrdinal("taraContenedor"));
+                                }
+
+                                lista.Add(mcontenedores);
+                            }
                         }
                     }
                 }
-            }
 
-            return lista;
+                return lista;
+            }
+            catch (Exception ex)
+            {
+               
+                throw new Exception("Error al listar contenedores", ex);
+            }
         }
 
+
+
+
+
     }
+
+
 }
